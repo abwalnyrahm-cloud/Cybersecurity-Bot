f301f74d2930b646a83dd93bd1788ae57020ebec
abwalnyrahm-cloud <abwalnyrahm@gmail.com>
2026-06-14 21:22:34 +0300

    Add main.py
    Fix command handling and update startup message.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-    print("Hello GitHub")
+  print("Cybersecurity Bot Started")
+                                             :while true
+command = input("Enter command: ")
+                     :"if command == "help
+Print("Avaiabe commands: help,
+                       scan, exit")
+    :"elif command =="scan
+      print("Goodye!") 
+                break
+                                    :else
+            print("Unknown command")
